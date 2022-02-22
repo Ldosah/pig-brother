9223b7207e3d2371a5df80318bb917397834d9f1
Ldosah <100087710+Ldosah@users.noreply.github.com>
2022-02-22 14:26:56 +0800

    Add files via upload
    公交车。。

---
+++ 公交车.py
@@ -0,0 +1,15 @@
+money=int(input("输入你有多少钱"))
+seat=int(input("输入有多少座位"))
+
+if money>=2:
+ print("可以上车")
+ 
+ if seat>=1:
+  print("有座位")
+ else:
+  print("没有座位")
+  
+else:
+ print("没钱上车")
+    
+    
